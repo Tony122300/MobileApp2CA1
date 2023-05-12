e1ef999786843b2374bf3fa2568d6ede7840e779
Tony JinHuiLiu <20090185@mail.wit.ie>
2023-05-12 15:41:40 +0100

    "Dark and light theme implemented"

--- app/src/main/java/ie/wit/caa/activities/Home.kt
+++ app/src/main/java/ie/wit/caa/activities/Home.kt
@@ -1,15 +1,23 @@
 package ie.wit.caa.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.location.Location
+//import android.location.Location
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
@@ -23,11 +31,12 @@ import ie.wit.caa.R
 import ie.wit.caa.databinding.HomeBinding
 import ie.wit.caa.databinding.NavHeaderMainBinding
 import ie.wit.caa.firebase.FirebaseImageManager
+import ie.wit.caa.firebaseui.FirebaseUIAuthManager
 import ie.wit.caa.ui.auth.LoggedInViewModel
 import ie.wit.caa.ui.auth.Login
-import ie.wit.caa.utils.customTransformation
-import ie.wit.caa.utils.readImageUri
-import ie.wit.caa.utils.showImagePicker
+import ie.wit.caa.ui.map.MapsViewModel
+//import ie.wit.caa.ui.map.MapsViewModel
+import ie.wit.caa.utils.*
 import timber.log.Timber
 
 class Home : AppCompatActivity() {
@@ -38,6 +47,7 @@ class Home : AppCompatActivity() {
     private lateinit var navHeaderMainBinding: NavHeaderMainBinding
     private lateinit var headerView : View
     private lateinit var intentLauncher : ActivityResultLauncher<Intent>
+    private val mapsViewModel :MapsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,41 +57,50 @@ class Home : AppCompatActivity() {
         drawerLayout = homeBinding.drawerLayout
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        loggedInViewModel = ViewModelProvider(this)[LoggedInViewModel::class.java]
+        loggedInViewModel.liveFirebaseUser.value = FirebaseUIAuthManager.auth.currentUser
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.reportCrimeActivityFragment, R.id.listFragment,R.id.mapsFragment2), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
         val navView = homeBinding.navView
         navView.setupWithNavController(navController)
         initNavHeader()
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.reportCrimeActivityFragment, R.id.listFragment
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
+        if(checkLocationPermissions(this)) {
+            mapsViewModel.updateCurrentLocation()
+        }
     }
 
     public override fun onStart() {
         super.onStart()
-        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
-        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
-            if (firebaseUser != null)
-                updateNavHeader(firebaseUser)
-        })
+//        loggedInViewModel = ViewModelProvider(this).get(LoggedInViewModel::class.java)
+//        loggedInViewModel.liveFirebaseUser.observe(this, Observer { firebaseUser ->
+//            if (firebaseUser != null)
+//                updateNavHeader(firebaseUser)
+//        })
+//
+//        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+//            if (loggedout) {
+//                startActivity(Intent(this, Login::class.java))
+//            }
+//        })
 
-        loggedInViewModel.loggedOut.observe(this, Observer { loggedout ->
+
+        Timber.i("EMAIL IS : %s", loggedInViewModel.liveFirebaseUser.value?.email)
+        loggedInViewModel.liveFirebaseUser.observe(this) { firebaseUser ->
+            if (firebaseUser != null)
+                updateNavHeader(loggedInViewModel.liveFirebaseUser.value!!)
+        }
+
+        loggedInViewModel.loggedOut.observe(this) { loggedout ->
             if (loggedout) {
                 startActivity(Intent(this, Login::class.java))
             }
-        })
-        registerImagePickerCallback()
-        navHeaderMainBinding.navHeaderImage.setOnClickListener {
-            showImagePicker(intentLauncher)
         }
-
+        registerImagePickerCallback()
     }
 
     private fun initNavHeader() {
@@ -95,38 +114,34 @@ class Home : AppCompatActivity() {
 
     }
 
+    fun onThemeSwitchClicked(item: MenuItem) {
+        val mode = if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            AppCompatDelegate.MODE_NIGHT_NO
+        } else {
+            AppCompatDelegate.MODE_NIGHT_YES
+        }
+        AppCompatDelegate.setDefaultNightMode(mode)
+        recreate()
+    }
+
 
     private fun updateNavHeader(currentUser: FirebaseUser) {
-        FirebaseImageManager.imageUri.observe(this) { result ->
-            if (result == Uri.EMPTY) {
-                Timber.i("CAA NO Existing imageUri")
-                if (currentUser.photoUrl != null) {
-                    //if you're a google user
-                    FirebaseImageManager.updateUserImage(
-                        currentUser.uid,
-                        currentUser.photoUrl,
-                        navHeaderMainBinding.navHeaderImage,
-                        false
-                    )
-                } else {
-                    Timber.i("CAA Loading Existing Default imageUri")
-                    FirebaseImageManager.updateDefaultImage(
-                        currentUser.uid,
-                        R.drawable.logo,
-                        navHeaderMainBinding.navHeaderImage
-                    )
-                }
-            } else {
-                Timber.i("CAA Loading Existing imageUri")
-                FirebaseImageManager.updateUserImage(
-                    currentUser.uid,
-                    FirebaseImageManager.imageUri.value,
-                    navHeaderMainBinding.navHeaderImage, false
-                )
-            }    }
-        navHeaderMainBinding.navHeaderEmail.text = currentUser.email
-        if(currentUser.displayName != null)
+        var headerView = homeBinding.navView.getHeaderView(0)
+        navHeaderMainBinding = NavHeaderMainBinding.bind(headerView)
+
+        if(currentUser.email != null) {
             navHeaderMainBinding.navHeaderName.text = currentUser.displayName
+            navHeaderMainBinding.navHeaderEmail.text = currentUser.email
+
+            if(currentUser.photoUrl != null)
+                Picasso.get().load(currentUser.photoUrl)
+                    .resize(200, 200)
+                    .transform(customTransformation())
+                    .centerCrop()
+                    .into(navHeaderMainBinding.navHeaderImage)
+        }
+        else
+            navHeaderMainBinding.navHeaderEmail.text = currentUser.phoneNumber
     }
 
         override fun onSupportNavigateUp(): Boolean {
@@ -134,12 +149,12 @@ class Home : AppCompatActivity() {
             return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
         }
 
-        fun signOut(item: MenuItem) {
-            loggedInViewModel.logOut()
-            val intent = Intent(this, Login::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
-        }
+    fun signOut(v: MenuItem) {
+        loggedInViewModel.signOut()
+        FirebaseUIAuthManager.auth.signOut()
+        finish()
+    }
+
 
     private fun registerImagePickerCallback() {
         intentLauncher =
@@ -159,5 +174,28 @@ class Home : AppCompatActivity() {
                 }
             }
     }
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (isPermissionGranted(requestCode, grantResults))
+            mapsViewModel.updateCurrentLocation()
+        else {
+            // permissions denied, so use a default location
+            mapsViewModel.currentLocation.value = Location("Default").apply {
+                latitude = 52.245696
+                longitude = -7.139102
+            }
+        }
+        Timber.i("LOC : %s", mapsViewModel.currentLocation.value)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.nav_drawer_menu, menu)
+        val switchItem = menu.findItem(R.id.theme_switch)
+        val switchTextView = switchItem.actionView?.findViewById<TextView>(R.id.theme_switch_textview)
+        switchTextView?.setOnClickListener { onThemeSwitchClicked(switchItem) }
+        return true
+    }
+
 
 }
